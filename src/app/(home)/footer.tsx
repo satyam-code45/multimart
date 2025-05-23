acda17c38f0bf9d085df42645ce3b040eeee9e3f
satyam-code45 <satyam45.dev@gmail.com>
2025-05-23 17:43:44 +0530

    setup home layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,10 @@
+
+export const Footer = () => {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+        <div className="flex items-center gap-2">
+            <p>MultiMart, Inc.</p>
+        </div>
+    </footer>
+  )
+}
---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "./footer";
+import { Navbar } from "./navbar";
+
+interface Props {
+  children: React.ReactNode;
+}
+function layout({ children }: Props) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-1 bg-[#F4F4F0]">
+        {children}
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default layout;

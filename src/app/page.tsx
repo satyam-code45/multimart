e7795aaba08f281a257679520ab2967554c77c69
satyam-code45 <satyam45.dev@gmail.com>
2025-05-23 11:05:08 +0530

    customized for Neobrutalism style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return(
-    <div>
-      <p className="text-rose-600">
-        Hello World
-      </p>
+    <div className="p-4">
+    <div className="flex flex-col gap-y-4 ">
+      <div>
+        <Button variant={"elevated"}>
+          I am a button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="I am an input"/>
+      </div>
+      <div>
+        <Progress value={50}/>
+      </div>
+      <div>
+        <Textarea>
+          I am textarea
+        </Textarea>
+      </div>
+      <div>
+        <Checkbox/>
+      </div>
+    </div>
     </div>
   ) 
 }

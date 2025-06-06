6f1852171bd12182f98843513f5b2107dfe87c58
satyam-code45 <satyam45.dev@gmail.com>
2025-06-06 18:49:04 +0530

    filtering products fix-1

--- src/modules/products/server/Procedures.ts
+++ src/modules/products/server/Procedures.ts
@@ -6,27 +6,29 @@ import { z } from "zod";
 export const productRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
-      z.object({ 
-      category: z.string().nullable().optional(),
-      minPrice: z.string().nullable().optional(),
-      maxPrice: z.string().nullable().optional(),
-    }))
+      z.object({
+        category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional(),
+      })
+    )
 
     .query(async ({ ctx, input }) => {
       const where: Where = {};
 
-      if (input.minPrice) {
-        where.price ={
-          ...where.price,
-          greater_than_equal: input.minPrice
-        }
-      }
-
-      if (input.maxPrice) {
-        where.price ={
-           ...where.price,
-          less_than_equal: input.maxPrice
-        }
+      if (input.minPrice && input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
+          greater_than_equal: input.minPrice,
+        };
+      } else if (input.minPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+        };
+      } else if (input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
+        };
       }
 
       if (input.category) {
